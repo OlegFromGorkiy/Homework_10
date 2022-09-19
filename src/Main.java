e354494569db50fb60124fc62c93f54f65c4f53a
Oleg Kartashev <officinator@gmail.com>
2022-09-19 20:39:20 +0300

    All tasks are complete!

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
         System.out.println("***\n");
         task_2(fullName);
         task_3(fullName);
+        //System.out.println(fullName);
     }
 
     static void task_2(String str) {
@@ -18,7 +19,7 @@ public class Main {
     }
 
     static void task_3(String str) {
-        System.out.println("Задание 2");
+        System.out.println("Задание 3");
         str = str.replace('ё', 'е');
         System.out.println("Данные ФИО сотрудника — " + str);
         System.out.println("***\n");
